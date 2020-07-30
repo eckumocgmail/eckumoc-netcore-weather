cc0d1683e1cafc9c30fc75820cb05646de8d1261
mister_kest <eckumoc@gmail.com>
2020-07-30 21:49:37 +0300

    Создан класс OpenWeatherAPIClient, который будет предоставлять информацию о погоде. Константин 30.07.2020

---
+++ APIS/OpenWeatherAPIClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eckumoc_netcore_weather.APIS
+{
+
+    // <summary>
+    // Класс реализует клиент общедоступного API для доступа к данным 
+    // о погоде https://api.openweathermap.org/.
+    // Идея класса в том, что доступные методы с модификатором доступа 
+    // public предоставляют информаю полученную с сервиса, полученную
+    // c помощью private-методами, реализующими HTTP-запросы 
+    // к сервису и предоставлющие данные идентичные тем, что полученны 
+    // в резутате выполнения запросов.
+    // <summary>    
+    public class OpenWeatherAPIClient
+    {
+
+    }
+}
