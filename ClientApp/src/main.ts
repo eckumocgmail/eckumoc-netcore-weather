b21b1f4162e5d4d790946e92f7eeb35dcf7255da
mister_kest <eckumoc@gmail.com>
2020-07-31 06:15:17 +0300

    Установил зависимость от библиотеки highcharts в приложение-клиент. Константин 31.07.2020

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -5,6 +5,20 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import '@angular/compiler';
+
+import * as Highcharts from 'highcharts';
+
+declare var require: any;
+const Boost = require('highcharts/modules/boost');
+const noData = require('highcharts/modules/no-data-to-display');
+const More = require('highcharts/highcharts-more');
+
+Boost(Highcharts);
+noData(Highcharts);
+More(Highcharts);
+noData(Highcharts);
+
 export function getBaseUrl() {
   return document.getElementsByTagName('base')[0].href;
 }
